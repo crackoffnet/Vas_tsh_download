81d68477de4aca5508507cbbdddb125d7efb8497
crackoffnet <crackoffnet@gmail.com>
2017-10-27 19:48:50 +0400

    Update run_startup_scripts.sh

--- run_startup_scripts.sh
+++ run_startup_scripts.sh
@@ -2,6 +2,7 @@
 
 exec nohup java -jar /var/www/html/vta-oar.jar > /var/log/vta-oar.log &
 exec nohup java -jar /var/www/html/vta-vas.jar > /var/log/vta-vas.log &
+exec nohup java -jar /var/www/html/vta-autokit.jar > /var/log/vta-autokit.log &
 /bin/sh /usr/local/tomcat/bin/startup.sh
 a2enconf servername
 service apache2 reload
